0004440e4f3a14ea8bb05146c1e04e631500e7fb
Arthur.WANG <fnghwsj@qq.com>
2014-11-26 15:24:29 +0800

    initial project configuration

---
+++ test/testSpec.js
@@ -0,0 +1,8 @@
+/**
+ * Created by ArthurWANG on 14/11/26.
+ */
+describe('hello world', function () {
+    it('should contains world', function () {
+        expect('Hello world').toContain('world');
+    });
+});
